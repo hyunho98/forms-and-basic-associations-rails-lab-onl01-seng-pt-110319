37806846801bebe5048d0cf67aad6fe862b2503d
Alex Lee <hyunho98@gmail.com>
2020-05-06 11:34:32 +0000

    Automatically backed up by Learn

--- db/migrate/20200506113241_add_notes_attributes_to_songs.rb
+++ db/migrate/20200506113241_add_notes_attributes_to_songs.rb
@@ -1,7 +1,7 @@
 class AddNotesAttributesToSongs < ActiveRecord::Migration[5.0]
   def change
     change_table :songs do |t|
-      t.string :notes_attributes
+      t.string :notes_attributes_0_content
     end
   end
 end
